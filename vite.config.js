f51e45b3cce3a2d112c8ae56704681143f870e3d
divyam110903 <117055743+divyam110903@users.noreply.github.com>
2025-02-15 23:52:37 +0530

    Fix base path for local and production

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: '/shuttlemanagement/',
+  base: process.env.NODE_ENV === 'production' ? '/shuttlemanagement/' : '/', // Use '/' for localhost
+  server: {
+    port: 5173,
+    open: true,
+  },
 })
